9d310e81722e46007ce35d65f1f649bf9d87f007
TinnyTomato <1872401992@qq.com>
2023-12-26 15:33:24 +0800

    First COmmit

---
+++ test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+def sonarcloud_test4zzy_bonus(t):  
+    a = 5
+    b = 10
+
+    if a != b:
+        print("True")
+        
+    if (a + b) != 20:
+        print("True")
+
+
+t = 100
+sonarcloud_test4zzy_bonus(t)
